ad22ceb2aa9f707b1c27a102b51bbab2b2af1177
Jeremy Elman <jelman@lbl.gov>
2013-11-06 13:52:38 -0800

    broadcasts data array to 2D and takes mean difference between primary net and all others

--- tools/net_affiliation.py
+++ tools/net_affiliation.py
@@ -4,8 +4,23 @@ import nibabel as nibabel
 from glob import glob
 import general_utilities as gu
 
+def get_dims(data):
+	"""
+	Given an array, returns dimensions necessary to broadcast to a 2D array.
 
-	
+	Input:
+	data : np.array 
+
+	Returns:
+	n_voxels : int
+			product of all axis shapes except the last
+	n_scans : int
+			equal to the shape of the last axis
+	"""
+	n_voxels = np.prod(data.shape[:-1])
+	n_scans = data.shape[-1]
+	return n_voxels, n_scans
+
 #Path to 4D files in which networks are concatenated over time
 datadir = '/home/jagust/rsfmri_ica/data/Allsubs_YoungICA_2mm_IC30.gica/dual_regress'
 #Directory to output difference maps
@@ -19,10 +34,12 @@ net_idx = [0,1,2,3,4,6,7,8,9,12,14,15,24,29]
 for subj_file in datafiles:
 	dat, aff = gu.load_nii(subj_file)
 	nets_dat = dat[:,:,:,net_idx]
-	n_voxels = np.prod(nets_dat.shape[:-1])
-	n_nets = nets_dat.shape[-1]
-	prim_net_idx = nets_dat.argmax(axis=3)
-	prim_net = nets_dat.max(axis=3)
-	other_nets = np.ma.array(nets_dat, mask=False)
+	nets_dat = np.atleast_2d(nets_dat)
+	n_voxels, n_nets = get_dims(nets_dat)
+	nets_dat2d = np.reshape(nets_dat, (n_voxels, n_nets))
+	prim_net_idx = nets_dat2d.argmax(axis=1)
+	prim_net = nets_dat2d.max(axis=1)
+	other_nets = np.ma.array(nets_dat2d, mask=False)
 	other_nets.mask[prim_net_idx] = True
 	nets_diff = prim_net - other_nets
+	nets_diff_mean = nets_diff.mean(axis=-1)
