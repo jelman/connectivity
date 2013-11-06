06678e11f8a55caee29def99c84d1074c28a4ca7
Jeremy Elman <jelman@lbl.gov>
2013-11-06 10:40:53 -0800

    begin script to create difference maps, determines whether each voxel is affiliated one or many networks

---
+++ tools/net_affiliation.py
@@ -0,0 +1,8 @@
+import os, sys
+import numpy as numpy
+import nibabel as nibabel
+
+
+datadir = '/home/jagust/rsfmri_ica/data/Allsubs_YoungICA_2mm_IC30.gica/dual_regress'
+outdir = '/home/jagust/rsfmri_ica/data/Allsubs_YoungICA_2mm_IC30.gica/difference_maps'
+
